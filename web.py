3b915b589f950d033543083901a253deef9f8553
Billy Vong <billy@mmo.me>
2011-10-18 20:49:57 -0700

    Add templates

--- web.py
+++ web.py
@@ -1,11 +1,12 @@
 import os
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
 
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Dongs!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
