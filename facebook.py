ebdf9f56fa5dcb1fbf5323d3cb344e336fe43ddc
Billy Vong <billy@mmo.me>
2011-11-09 22:40:24 -0800

    Some login stuff.

--- facebook.py
+++ facebook.py
@@ -4,12 +4,15 @@ FACEBOOK_APP_ID = os.environ.get("FACEBOOK_APP_ID")
 FACEBOOK_APP_SECRET = os.environ.get("FACEBOOK_APP_SECRET")
 
 def get_cookie(request):
-    cookie = request.cookies.get('fbs_%s' % FACEBOOK_APP_ID)
-    fb_dict = dict(item.split('=') for item in cookie.split('&'))
-    cookie_sig = fb_dict['sig']
-    del fb_dict['sig']
+    try:
+        cookie = request.cookies.get('fbs_%s' % FACEBOOK_APP_ID)
+        fb_dict = dict(item.split('=') for item in cookie.split('&'))
+        cookie_sig = fb_dict['sig']
+        del fb_dict['sig']
 
-    if hashlib.md5(''.join(['%s=%s' % (k, v) for k, v in sorted(fb_dict.iteritems())]) + FACEBOOK_APP_SECRET).hexdigest() == cookie_sig:
-        return fb_dict
-    else:
-        raise Exception('Cookie is not valid')
+        if hashlib.md5(''.join(['%s=%s' % (k, v) for k, v in sorted(fb_dict.iteritems())]) + FACEBOOK_APP_SECRET).hexdigest() == cookie_sig:
+            return fb_dict
+        else:
+            raise Exception('Cookie is not valid')
+    except:
+        raise Exception('Cookie not found')
