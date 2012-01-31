fb01d12fbf5898ad67251c51df2d7928da225b4d
Billy Vong <billyvg@gmail.com>
2012-01-30 20:25:07 -0800

    Revert "Basic implementation of emberjs (replaced Backbone.)"
    This reverts commit ca7849aa5d868628f8d54782be1c1c6bf4c8eac3.
    
    Conflicts:
    
    	static/budjet/js/app.js

--- static/budjet/js/backbone/models/transaction.js
+++ static/budjet/js/backbone/models/transaction.js
@@ -1,13 +1,25 @@
-BJ.Transaction = Em.Object.extend({
-  amount: 0,
-  description: '',
-  type: '',
-  reoccuring: false,
-
-  relative: function() {
+BJ.TransactionModel = Backbone.Model.extend({
+  defaults: function() {
+    return {
+      amount: 0,
+      description: '',
+      type: '',
+      reoccuring: false
+    }
+  }
+  /**
+   * Retrieve the relative value of the transaction (i.e.
+   * negative for an expense, and positive for income).
+   */
+, relative: function() {
     if (this.get('type') == 'expense') {
       return this.get('amount')*-1;
     }
-    return this.get('amount');
-  }.property('type', 'amount')
+
+    return this.get('amount')
+  }
+});
+
+BJ.TransactionCollection = Backbone.Collection.extend({
+  model: BJ.TransactionModel,
 });
--- static/budjet/js/backbone/views/transaction.js
+++ static/budjet/js/backbone/views/transaction.js
@@ -1,6 +1,11 @@
-BJ.TransactionView = Em.View.extend({
-  idBinding: 'content.id'
-});
+BJ.TransactionView = Backbone.View.extend({
+  tagName: 'tr'
 
-BJ.TransactionListView = Em.View.extend({
-});
+, render: function() {
+    var template = '<td>1</td> <td>1/1/2011</td> <td>$100</td> <td>Temporary Description</td> <td>Yes</td>'
+    $(this.el).html(template)
+    return this.el;
+  }
+
+
+})
