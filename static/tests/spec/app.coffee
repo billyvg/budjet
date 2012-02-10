28104c4b3dc7a57a49bcd76d4e68c22f28915baf
Billy Vong <billyvg@gmail.com>
2012-02-09 21:37:54 -0800

    Add some more tests for transactions.

--- static/tests/spec/app.coffee
+++ static/tests/spec/app.coffee
@@ -1,83 +1,101 @@
-describe 'Tests for transactions models.', () ->
+namespace 'BJ', (exports) ->
+  describe 'Tests for Transaction Model.', () ->
+    beforeEach () ->
+      @income = new exports.Transaction {
+        amount: 100
+        description: 'Testing Income'
+        reoccuring: true
+      }
+      @expense = new exports.Transaction {
+        amount: -100
+        description: 'Testing expense'
+        reoccuring: true
+      }
 
-  beforeEach () ->
-    @income = new BJ.Transaction {
-      amount: 100
-      description: 'Testing Income'
-      reoccuring: true
-    }
-    @expense = new BJ.Transaction {
-      amount: -100
-      description: 'Testing expense'
-      reoccuring: true
-    }
-    ''
+    it 'Can be created with default values', () ->
+      transaction = new exports.Transaction
+      expect(transaction.get('amount')).toBe 0
+      expect(transaction.get('description')).toBe ''
+      expect(transaction.get('reoccuring')).toBe false
 
-  it 'Can be created with default values', () ->
-    transaction = new BJ.Transaction
-    expect transaction.get('amount').toBe 0
-    expect transaction.get('description').toBe ''
-    expect transaction.get('reoccuring').toBe false
-    ''
+    it 'Can be created with set values', () ->
+      transaction = new exports.Transaction {
+        amount: 100
+        description: 'Testing'
+        reoccuring: true
+      }
+      expect(transaction.get('amount')).toBe 100
+      expect(transaction.get('description')).toBe 'Testing'
+      expect(transaction.get('reoccuring')).toBe true
 
-  it 'Can be created with set values', () ->
-    transaction = new BJ.Transaction {
-      amount: 100
-      description: 'Testing'
-      reoccuring: true
-    }
-    expect transaction.get('amount')  .toBe 100
-    expect transaction.get('description')  .toBe 'Testing'
-    expect transaction.get('reoccuring')  .toBe true
-    ''
+    it 'Is an "expense" if amount is negative', () ->
+      expect(@expense.type()).toBe 'expense'
 
-  it 'Is an "expense" if amount is negative', () ->
-    expect @expense.get('type') .toBe 'expense'
-    ''
+    it 'Is "income" if amount is positive', () ->
+      expect(@income.type()).toBe 'income'
 
-  it 'Is "income" if amount is positive', () ->
-    expect @income.get('type') .toBe 'income'
-    ''
+    it 'Is empty string if amount is 0', () ->
+      @income.set {amount: 0}
+      expect(@income.type()).toBe ''
 
-  it 'Is empty string if amount is 0', () ->
-    @income.set 'amount', 0
-    expect @income.get('type') .toBe ''
-    ''
+  describe 'Tests for Transaction Collection', () ->
 
-  ''
+    beforeEach () ->
+      @transactions = new exports.Transactions
+      # @transactions destroy
 
-describe 'Tests for transactionsController', () ->
+    it 'Add a new transaction with non-default values to collection ', () ->
+      trans =
+        date: ''
+        amount: 100
+        description: 'test'
+        reoccuring: true
 
-  beforeEach () ->
-    @controller = new BJ.transactionsController
-    @controller.set 'content', []
-    ''
+      expect(@transactions.models.lenth).toBe undefined
+      @transactions.add trans
+      expect(@transactions.models.length).toBe 1
 
-  it 'Should be sum of all the transactions',  () ->
-    transactions = [-100, 50, 50, -20, 10, 0]
+      content = @transactions.models[0]
 
-    @controller.pushObject new BJ.Transaction {amount: transaction} for own i, transaction of intransactions
+      expect(content.get 'date' ).toBe trans.date
+      expect(content.get 'amount' ).toBe trans.amount
+      expect(content.get 'description' ).toBe trans.description
+      expect(content.get 'reoccuring' ).toBe trans.reoccuring
 
-    expect(@controller.totalAssets()).toBe -10
+    it 'Add transactions with different amounts to the collection and return the sum of all the transactions', () ->
+      transactions = [-100, 50, 50, -20, 10, 0]
+      sum = 0
 
-    ''
+      @transactions.add {amount: transaction} for transaction in transactions
+      sum = sum + transaction for transaction in transactions
+      expect(@transactions.total()).toBe sum
 
-  it 'addTransaction should create a new Transaction in controllers content property', () ->
-    trans = 
-      date: ''
-      amount: 100
-      description: 'test'
-      reoccuring: true
+    it 'Calculate the total amount of reoccurring transactions.', () ->
+      expect('').toBe false
 
-    expect(@controller.content.length).toBe 0
-    @controller.addTransaction trans
-    expect(@controller.content.length).toBe 1
+    it 'Calculate the available money in a certain time period.', () ->
+      expect('').toBe false
 
-    content = @controller.content[0]
+  describe 'Tests of the Summary View', () ->
+    beforeEach () ->
+      @transactions add {
+        amount: 100
+        reoccuring: 14
+      }
 
-    expect(content.get 'date' ).toBe trans.date
-    expect(content.get 'amount' ).toBe trans.amount
-    expect(content.get 'description' ).toBe trans.description
-    expect(content.get 'reoccuring' ).toBe trans.reoccuring
-    ''
-  ''
+    it 'Check if reoccurring sum updates when a transaction is added.', () ->
+      expect('').toBe false
+
+    it 'Check if reoccurring sum updates when a transaction is removed.', () ->
+      expect('').toBe false
+
+    it 'Check if non-reoccurring sum updates when a transaction is added.', () ->
+      expect('').toBe false
+
+    it 'Check if non-reoccurring sum updates when a transaction is removed.', () ->
+      expect('').toBe false
+
+
+
+
+
--- static/tests/spec/app.js
+++ static/tests/spec/app.js
@@ -1,88 +1,114 @@
-describe('Tests for transactions models.', function() {
+(function() {
 
-  beforeEach(function() {
-    this.income = BJ.Transaction.create({
-      amount: 100,
-      description: 'Testing Income',
-      reoccuring: true
+  namespace('BJ', function(exports) {
+    describe('Tests for Transaction Model.', function() {
+      beforeEach(function() {
+        this.income = new exports.Transaction({
+          amount: 100,
+          description: 'Testing Income',
+          reoccuring: true
+        });
+        return this.expense = new exports.Transaction({
+          amount: -100,
+          description: 'Testing expense',
+          reoccuring: true
+        });
+      });
+      it('Can be created with default values', function() {
+        var transaction;
+        transaction = new exports.Transaction;
+        expect(transaction.get('amount')).toBe(0);
+        expect(transaction.get('description')).toBe('');
+        return expect(transaction.get('reoccuring')).toBe(false);
+      });
+      it('Can be created with set values', function() {
+        var transaction;
+        transaction = new exports.Transaction({
+          amount: 100,
+          description: 'Testing',
+          reoccuring: true
+        });
+        expect(transaction.get('amount')).toBe(100);
+        expect(transaction.get('description')).toBe('Testing');
+        return expect(transaction.get('reoccuring')).toBe(true);
+      });
+      it('Is an "expense" if amount is negative', function() {
+        return expect(this.expense.type()).toBe('expense');
+      });
+      it('Is "income" if amount is positive', function() {
+        return expect(this.income.type()).toBe('income');
+      });
+      return it('Is empty string if amount is 0', function() {
+        this.income.set({
+          amount: 0
+        });
+        return expect(this.income.type()).toBe('');
+      });
     });
-    this.expense = BJ.Transaction.create({
-      amount: -100,
-      description: 'Testing expense',
-      reoccuring: true
+    describe('Tests for Transaction Collection', function() {
+      beforeEach(function() {
+        return this.transactions = new exports.Transactions;
+      });
+      it('Add a new transaction with non-default values to collection ', function() {
+        var content, trans;
+        trans = {
+          date: '',
+          amount: 100,
+          description: 'test',
+          reoccuring: true
+        };
+        expect(this.transactions.models.lenth).toBe(void 0);
+        this.transactions.add(trans);
+        expect(this.transactions.models.length).toBe(1);
+        content = this.transactions.models[0];
+        expect(content.get('date')).toBe(trans.date);
+        expect(content.get('amount')).toBe(trans.amount);
+        expect(content.get('description')).toBe(trans.description);
+        return expect(content.get('reoccuring')).toBe(trans.reoccuring);
+      });
+      it('Add transactions with different amounts to the collection and return the sum of all the transactions', function() {
+        var sum, transaction, transactions, _i, _j, _len, _len2;
+        transactions = [-100, 50, 50, -20, 10, 0];
+        sum = 0;
+        for (_i = 0, _len = transactions.length; _i < _len; _i++) {
+          transaction = transactions[_i];
+          this.transactions.add({
+            amount: transaction
+          });
+        }
+        for (_j = 0, _len2 = transactions.length; _j < _len2; _j++) {
+          transaction = transactions[_j];
+          sum = sum + transaction;
+        }
+        return expect(this.transactions.total()).toBe(sum);
+      });
+      it('Calculate the total amount of reoccurring transactions.', function() {
+        return expect('').toBe(false);
+      });
+      return it('Calculate the available money in a certain time period.', function() {
+        return expect('').toBe(false);
+      });
+    });
+    return describe('Tests of the Summary View', function() {
+      beforeEach(function() {
+        return this.transactions(add({
+          amount: 100,
+          reoccuring: 14
+        }));
+      });
+      it('Check if reoccurring sum updates when a transaction is added.', function() {
+        return expect('').toBe(false);
+      });
+      it('Check if reoccurring sum updates when a transaction is removed.', function() {
+        return expect('').toBe(false);
+      });
+      it('Check if non-reoccurring sum updates when a transaction is added.', function() {
+        return expect('').toBe(false);
+      });
+      return it('Check if non-reoccurring sum updates when a transaction is removed.', function() {
+        return expect('').toBe(false);
+      });
     });
   });
 
-  it('Can be created with default values', function() {
-    var transaction = BJ.Transaction.create();
-    expect(transaction.get('amount')).toBe(0);
-    expect(transaction.get('description')).toBe('');
-    expect(transaction.get('reoccuring')).toBe(false);
-  });
-
-  it('Can be created with set values', function() {
-    var transaction = BJ.Transaction.create({
-      amount: 100,
-      description: 'Testing',
-      reoccuring: true
-    });
-    expect(transaction.get('amount')).toBe(100);
-    expect(transaction.get('description')).toBe('Testing');
-    expect(transaction.get('reoccuring')).toBe(true);
-  });
-
-  it('Is an "expense" if amount is negative', function() {
-    expect(this.expense.get('type')).toBe('expense');
-  });
-
-  it('Is "income" if amount is positive', function() {
-    expect(this.income.get('type')).toBe('income');
-  });
-
-  it('Is empty string if amount is 0', function() {
-    this.income.set('amount', 0);
-    expect(this.income.get('type')).toBe('');
-  });
-});
-
-
-describe('Tests for transactionsController', function() {
-
-  beforeEach(function() {
-    this.controller = BJ.transactionsController;
-    this.controller.set('content', []);
-  });
-
-
-  it('Should be sum of all the transactions', function() {
-    var transactions = [-100, 50, 50, -20, 10, 0];
-
-    for (var x = 0; x < transactions.length; x++) {
-      this.controller.pushObject(BJ.Transaction.create({amount: transactions[x]}));
-    }
-    expect(this.controller.totalAssets()).toBe(-10);
-  });
-
-  it('addTransaction should create a new Transaction in controllers content property',
-    function() {
-      var trans = {
-        date: '',
-        amount: 100,
-        description: 'test',
-        reoccuring: true
-      };
-
-      expect(this.controller.content.length).toBe(0);
-      this.controller.addTransaction(trans);
-      expect(this.controller.content.length).toBe(1);
-
-      var content = this.controller.content[0];
-
-      expect(content.get('date')).toBe(trans.date);
-      expect(content.get('amount')).toBe(trans.amount);
-      expect(content.get('description')).toBe(trans.description);
-      expect(content.get('reoccuring')).toBe(trans.reoccuring);
-    }
-  );
-
-});
+}).call(this);
