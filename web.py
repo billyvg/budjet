420ba8685b538180ab76eab48ed5eefc24641433
Billy Vong <billyvg@gmail.com>
2012-02-12 21:28:23 -0800

    Adding my resume to this because I'm lazy.

--- web.py
+++ web.py
@@ -23,6 +23,10 @@ def index():
         print sys.exc_info()
     return render_template("index.html", fb_user=fb_user)
 
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
 @app.route("/jasmine")
 def jasmine_test():
     return render_template("tests/jasmine.html")
